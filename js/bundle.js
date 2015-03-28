6f1fd47afad2f1825414c06bcb6aa97d9ab7a03a
benji6 <benhall22@hotmail.co.uk>
2015-03-28 23:39:58 +0000

    oops

--- js/bundle.js
+++ js/bundle.js
@@ -1,9 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+const R = require('ramda');
+
 const TicTacToe = require('./tic-tac-toe/controller.js');
 
 TicTacToe(R.repeat(0, 9));
 
-},{"./tic-tac-toe/controller.js":2}],2:[function(require,module,exports){
+},{"./tic-tac-toe/controller.js":2,"ramda":6}],2:[function(require,module,exports){
 const renderBoard = require('./renderBoard');
 const renderMessage = require('./renderMessage.js');
 const R = require('ramda');
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const R = require('ramda');
+
 const TicTacToe = require('./tic-tac-toe/controller.js');
 
 TicTacToe(R.repeat(0, 9));
