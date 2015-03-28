00ec18575dfa192dd4d58d312bc8ad9f00ebc332
benji6 <benhall22@hotmail.co.uk>
2015-03-28 23:16:20 +0000

    using patch and diff for rendering successive views

--- js/bundle.js
+++ js/bundle.js
@@ -87,18 +87,35 @@ const createElement = require('virtual-dom/create-element');
 const createRow = R.curry(h)('tr', R.__, undefined);
 const createCell = R.curry(h)('td', R.__, R.__);
 
+const createVirtualRoot = (rows, onclick) => h('div.center', [
+  h('table', R.mapIndexed((row, rowIndex) => createRow(R.mapIndexed((cell, cellIndex) => createCell({
+    onclick: () => onclick(R.add(R.multiply(rowIndex, R.length(row)), cellIndex))
+  }, getCharacterFromModelCode(cell)), row)), rows))
+]);
+
 const getCharacterFromModelCode = (code) => R.cond(
   [R.eq(0), R.always('')],
   [R.eq(1), R.always('O')],
   [R.eq(2), R.always('X')]
 )(code);
 
+var virtualRoot = null;
+var domRoot = null;
+
 module.exports = (rows, onclick) => {
-  document.getElementById('board_container').appendChild(createElement(h('div.center', [
-    h('table', R.mapIndexed((row, rowIndex) => createRow(R.mapIndexed((cell, cellIndex) => createCell({
-      onclick: () => onclick(R.add(R.multiply(rowIndex, R.length(row)), cellIndex))
-    }, getCharacterFromModelCode(cell)), row)), rows))
-  ])));
+  R.ifElse(
+    R.not,
+    () => {
+      virtualRoot = createVirtualRoot(rows, onclick);
+      domRoot = createElement(virtualRoot);
+      document.getElementById('board_container').appendChild(domRoot);
+    },
+    () => {
+      const newVirtualRoot = createVirtualRoot(rows, onclick);
+      domRoot = patch(domRoot, diff(virtualRoot, createVirtualRoot(rows, onclick)));
+      virtualRoot = newVirtualRoot;
+    }
+  )(domRoot);
 };
 
 },{"ramda":7,"virtual-dom/create-element":8,"virtual-dom/diff":9,"virtual-dom/h":10,"virtual-dom/patch":18}],5:[function(require,module,exports){
