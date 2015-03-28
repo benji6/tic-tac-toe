0a8ad12be7c2f2bbccb5b4b72c84e4af932eaf7a
benji6 <benhall22@hotmail.co.uk>
2015-03-28 16:45:06 +0000

    forgot this file

---
+++ js/tic-tac-toe/messageView.js
@@ -0,0 +1,25 @@
+const jsmlParse = require('jsml-parse');
+
+const createJsml = function (text = '') {
+  return {
+    tag: "div",
+    className: "center",
+    children: {
+      tag: "output",
+      text
+    }
+  };
+};
+
+const renderMesssage = (message) =>
+  jsmlParse(createJsml(message), document.getElementById('message_container'));
+
+const renderVictoryMessage = (winner) =>
+  renderMesssage(`Victory for ${winner}!`);
+
+const renderDrawMessage = () => renderMesssage(`Draw`);
+
+module.exports = {
+  renderVictoryMessage,
+  renderDrawMessage
+};
