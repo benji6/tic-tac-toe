3a6ec1250afc79576e9eeaadd502d69ced1d1c7b
benji6 <benhall22@hotmail.co.uk>
2015-03-28 23:22:37 +0000

    tidy up

--- js/bundle.js
+++ js/bundle.js
@@ -123,15 +123,13 @@ const R = require('ramda');
 const h = require('virtual-dom/h');
 const createElement = require('virtual-dom/create-element');
 
-const draw = () =>
+const renderMessage = (message) =>
   document.getElementById('message_container').appendChild(createElement(h('div.center', [
-    h('output', 'Draw')
+    h('output', message)
   ])));
 
-const victory = (winner) =>
-  document.getElementById('message_container').appendChild(createElement(h('div.center', [
-    h('output', `Victory for ${winner}!`)
-  ])));
+const draw = () => renderMessage(`Draw`);
+const victory = (winner) => renderMessage(`Victory for ${winner}!`);
 
 module.exports = {
   draw,
