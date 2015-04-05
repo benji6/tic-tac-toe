fc21bd6d2c373a961af0602df1d3814f6e5140a2
benji6 <benhall22@hotmail.co.uk>
2015-04-06 00:42:26 +0100

    working on computing victory

--- js/tic-tac-toe/controller.js
+++ js/tic-tac-toe/controller.js
@@ -5,37 +5,16 @@ const renderBoard = require('./renderBoard');
 const renderMessage = require('./renderMessage.js');
 const Y = require('./Y.js');
 
-
-
-
-// const getDiagonalsIndices = (boardModel) => R.concat(
-//   [R.map((index) =>
-//     R.multiply(index, R.add(computeRowAndColumnCount(boardModel), 1)), R.range(0, computeRowAndColumnCount(boardModel)))],
-//   [R.map((index) =>
-//     R.multiply(R.inc(index), R.subtract(computeRowAndColumnCount(boardModel), 1)), R.range(0, computeRowAndColumnCount(boardModel)))]
-// );
-// const getDiagonals = (boardModel) =>
-//   R.map((diagonals) =>
-//   R.map((value) =>
-//   boardModel[value], diagonals), getDiagonalsIndices(boardModel));
-// const isGameOver = (boardModel) => R.or(isVictory(boardModel), boardIsFull(boardModel));
-// const isThreeInARow = (line) => R.or(R.all(equalsOne, line), R.all(equalsTwo, line));
-// const isValidMove = R.curry((boardModel, index) =>
-//   R.and(equalsZero(boardModel[index]), R.not(isGameOver(boardModel))));
-// const isVictory = (boardModel) => R.any(isThreeInARow, R.concat(R.concat(getRows(boardModel),
-//   getColumns(boardModel)),
-//   getDiagonals(boardModel)));
-
 module.exports = Y((recurse) => (boardData) => {
   const model = createModel(boardData);
-console.log(model.computePlayerTurn());
+
   renderBoard(model.getRows(), (index) => R.both(
     model.isValidMove,
     (index) => recurse(model.computeNewModel(index, model.computePlayerTurn()))
   )(index));
 
-  // R.cond(
-  //   [isVictory, (boardModel) => renderMessage.victory(equalsOne(computeLastPlayerTurn(boardModel)) ? "noughts" : "crosses")],
-  //   [boardIsFull, renderMessage.draw]
-  // )(boardModel);
+  R.cond(
+    [model.isVictory, () => renderMessage.victory(R.eq(1, model.computeLastPlayerTurn()) ? "noughts" : "crosses")]
+    // [boardIsFull, renderMessage.draw]
+  )();
 });
--- js/tic-tac-toe/createModel.js
+++ js/tic-tac-toe/createModel.js
@@ -5,18 +5,41 @@ const equalsZero = R.eq(0);
 const equalsOne = R.eq(1);
 const equalsTwo = R.eq(2);
 
+
+// const getDiagonalsIndices = (boardModel) => R.concat(
+//   [R.map((index) =>
+//     R.multiply(index, R.add(computeRowAndColumnCount(boardModel), 1)), R.range(0, computeRowAndColumnCount(boardModel)))],
+//   [R.map((index) =>
+//     R.multiply(R.inc(index), R.subtract(computeRowAndColumnCount(boardModel), 1)), R.range(0, computeRowAndColumnCount(boardModel)))]
+// );
+// const getDiagonals = (boardModel) =>
+//   R.map((diagonals) =>
+//   R.map((value) =>
+//   boardModel[value], diagonals), getDiagonalsIndices(boardModel));
+// const isGameOver = (boardModel) => R.or(isVictory(boardModel), boardIsFull(boardModel));
+
+// const isValidMove = R.curry((boardModel, index) =>
+//   R.and(equalsZero(boardModel[index]), R.not(isGameOver(boardModel))));
+
+
+
 module.exports = (model) => {
   const countOfCellsWhere = m.comp(m.count, R.curry(R.flip(R.binary(m.filter)))(m.flatten(model)));
   const computePlayerTurn = () => R.ifElse(m.curry(R.eq, countOfCellsWhere(equalsOne)), R.always(1), R.always(2))(countOfCellsWhere(equalsTwo));
-  const computeLastPlayerTurn = () => R.ifElse(m.compose(equalsOne(computePlayerTurn)), R.always(2), R.always(1))();
-
+  const computeLastPlayerTurn = R.ifElse(m.comp(equalsOne, computePlayerTurn), R.always(2), R.always(1));
+console.log(computeLastPlayerTurn()()()()()());
   const boardIsFull = () => equalsZero(filteredLength(equalsZero));
   const getRows = () => m.intoArray(m.map(m.intoArray, model));
 
   const getColumnFromIndex = (index) => R.modulo(index, m.count(model));
   const getRowFromIndex = (index) => Math.floor(R.divide(index, m.count(model)));
-
+  const isThreeInARow = (line) => R.or(m.every(equalsOne, line), m.every(equalsTwo, line));
   const isValidMove = (index) => equalsZero(m.nth(m.nth(model, getRowFromIndex(index)), getColumnFromIndex(index)));
+
+  const isVictory = () => m.some(isThreeInARow, model);
+  // R.any(isThreeInARow, R.concat(R.concat(getRows(boardModel),
+  //   getColumns(boardModel)),
+  //   getDiagonals(boardModel)));
 
   const computeNewModel = (index, value) =>
    m.updateIn(model, [getRowFromIndex(index)], R.always(m.updateIn(m.nth(model, getRowFromIndex(index)), [getColumnFromIndex(index)], R.always(value))));
@@ -25,8 +48,10 @@ module.exports = (model) => {
     boardIsFull,
     computeNewModel,
     computePlayerTurn,
+    computeLastPlayerTurn,
     getRows,
-    isValidMove
+    isValidMove,
+    isVictory
   };
 
 };
